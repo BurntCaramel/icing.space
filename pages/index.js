899ecdd145047f32fa2409925175c1ee5d162cef
Patrick Smith <patrick@burntcaramel.com>
2018-01-19 10:15:08 +1100

    Improve wording

--- pages/index.js
+++ pages/index.js
@@ -17,7 +17,7 @@ export default ({
   <a href="https://fruitbat.collected.design/" className=""><h2 className="">Fruitbat: Plan your migrations for Rails and Ecto</h2></a>
   <a href="https://collected.design/" className=""><h2 className="">Collected: a design system from messaging to components</h2></a>
   <a href="https://lofi.design/" className=""><h2 className="">Lofi: content’s new best friend</h2></a>
-  <a href="https://lokum.icing.space/" className=""><h2 className="">Lokum: turns Trello boards into websites</h2></a>
+  <a href="https://lokum.icing.space/" className=""><h2 className="">Lokum: make websites directly from Trello boards</h2></a>
   <a href="https://github.com/RoyalIcing" className=""><h2 className="">GitHub</h2><p>Everything is open source for your team to use</p></a>
   <a href="https://twitter.com/RoyalIcing" className=""><h2 className="">Twitter</h2></a>
   <a href="https://medium.com/the-tech-bench" className=""><h2 className="">The Tech Bench</h2><p>Guides on React, Elm, Swift, Node.js, and application programming</p></a></article></main>
