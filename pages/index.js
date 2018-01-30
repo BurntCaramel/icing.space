40739547d6824cb1f2dfedf14e0ee212860bea96
Patrick Smith <patrick@burntcaramel.com>
2018-01-31 00:57:56 +1100

    Add link to What the App

--- pages/index.js
+++ pages/index.js
@@ -13,13 +13,14 @@ export default ({
     </Head>
     <main className="content"><header ><h1 >Make products? We make open source tools for you</h1></header>
 <article >
-  <a href="https://datadown.netlify.com/" className=""><h2 className="">Datadown: Use Markdown to play with data and components</h2></a>
-  <a href="https://fruitbat.collected.design/" className=""><h2 className="">Fruitbat: Plan your migrations for Rails and Ecto</h2></a>
-  <a href="https://collected.design/" className=""><h2 className="">Collected: a design system from messaging to components</h2></a>
-  <a href="https://lofi.design/" className=""><h2 className="">Lofi: content’s new best friend</h2></a>
-  <a href="https://lokum.icing.space/" className=""><h2 className="">Lokum: make websites directly from Trello boards</h2></a>
-  <a href="https://github.com/RoyalIcing" className=""><h2 className="">GitHub</h2><p>Everything is open source for your team to use</p></a>
-  <a href="https://twitter.com/RoyalIcing" className=""><h2 className="">Twitter</h2></a>
-  <a href="https://medium.com/the-tech-bench" className=""><h2 className="">The Tech Bench</h2><p>Guides on React, Elm, Swift, Node.js, and application programming</p></a></article></main>
+  <a href="https://datadown.netlify.com/"><h2>Datadown: Use Markdown to play with data and components</h2></a>
+  <a href="https://fruitbat.collected.design/"><h2>Fruitbat: Plan your migrations for Rails and Ecto</h2></a>
+  <a href="https://whattheapp.netlify.com/"><h2>What the App: Understand the world of technology in words written by people</h2></a>
+  <a href="https://collected.design/"><h2>Collected: a design system from messaging to components</h2></a>
+  <a href="https://lofi.design/"><h2>Lofi: content’s new best friend</h2></a>
+  <a href="https://lokum.icing.space/"><h2>Lokum: make websites directly from Trello boards</h2></a>
+  <a href="https://github.com/RoyalIcing"><h2>GitHub</h2><p>Everything is open source for your team to use</p></a>
+  <a href="https://twitter.com/RoyalIcing"><h2>Twitter</h2></a>
+  <a href="https://medium.com/the-tech-bench"><h2>The Tech Bench</h2><p>Guides on React, Elm, Swift, Node.js, and application programming</p></a></article></main>
   </div>
 )
